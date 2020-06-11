a9e788f4e846bfb3ab6db836d081d2c15a321fb7
mfathur <mfathur17@gmail.com>
2020-06-11 23:53:18 +0700

    set Coordinatorlayout+fab in spreadDetail & fix some view in other fragments

--- app/src/main/java/com/mfathurz/wartacovid19/ui/SpreadDetailFragment.kt
+++ app/src/main/java/com/mfathurz/wartacovid19/ui/SpreadDetailFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.mfathurz.wartacovid19.R
+import kotlinx.android.synthetic.main.fragment_spread_detail.*
 
 class SpreadDetailFragment : Fragment() {
 
@@ -15,6 +17,21 @@ class SpreadDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_spread_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        detailRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (dy > 0 && fab_toGlobalSpreadDetail.visibility == View.VISIBLE) {
+                    fab_toGlobalSpreadDetail.hide();
+                } else if (dy < 0 && fab_toGlobalSpreadDetail.visibility != View.VISIBLE) {
+                    fab_toGlobalSpreadDetail.show();
+                }
+            }
+        })
     }
 
 }
