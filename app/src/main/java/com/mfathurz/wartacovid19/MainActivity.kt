4b2d03103eafadc6ce9775e68055ceb1d1bcdf83
mfathur <mfathur17@gmail.com>
2020-06-19 08:19:21 +0700

    change navigation bar color and add palette api

--- app/src/main/java/com/mfathurz/wartacovid19/MainActivity.kt
+++ app/src/main/java/com/mfathurz/wartacovid19/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mfathurz.wartacovid19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,5 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navController= Navigation.findNavController(this,R.id.navFragmentContainer)
         NavigationUI.setupWithNavController(mBottomNav,navController)
+
+
     }
 }
